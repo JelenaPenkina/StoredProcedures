99902072739b103bc43a173fb2dc2836f8e6d554
Opilane TTHK <122533981+JelenaPenkina@users.noreply.github.com>
2025-06-04 13:26:39 +0300

    04.06.2025
    Andmebaasid

--- StoredProcedure/StoredProcedure/Controllers/EmpolyeeController.cs
+++ StoredProcedure/StoredProcedure/Controllers/EmpolyeeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using StoredProcedure.Data;
 using StoredProcedure.Models;
@@ -33,6 +34,35 @@ public class EmployeeController : Controller
             .ToList();
 
         return result;
+    }
+
+    [HttpGet]
+    public IActionResult DynamicSql()
+    {
+        string connectionStr = _configuration.GetConnectionString("DeafaultConnection");
+
+        using (SqlConnection con = new SqlConnection(connectionStr))
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "dbo.spSearchEmployees";
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            con.Open();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            List<Employee> model = new List<Employee>();
+            while (sdr.Read())
+            {
+                var details = new Employee();
+                details.FirstName = sdr["FirstName"].ToString();
+                details.LastName = sdr["LastName"].ToString();
+                details.Gender = sdr["Gender"].ToString();
+                details.Salary = Convert.ToInt32(sdr["Salary"]);
+                model.Add(details);
+            }
+            return View(model);
+
+        }
+
     }
 
 }
